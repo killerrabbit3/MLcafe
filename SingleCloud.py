d623f015f1d235f7351e02f9cd9ac6f316e9a50d
alexander <stonemaa@nmsu.edu>
2020-01-29 14:40:43 -0700

    Created new project for ML cafe

---
+++ SingleCloud.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import tensorflow as tf
+data2803 = pd.read_csv('Data1/MgII2803data.txt', delimiter=' ', header = None)
+data2796 = pd.read_csv('Data1/MgII2796data.txt', delimiter=' ', header = None)
+labels = pd.read_csv('Data1/labels.txt', delimiter=' ', header = None)
+
+
+
